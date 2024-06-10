0d0df512546656bab80c6ce0de3e5645c7c03e15
Rahul Kumar <rahulkhg980@gmail.com>
2024-06-10 14:43:16 +0530

    Updating routes

---
+++ Frontend/src/pages/Mainpage.tsx
@@ -0,0 +1,12 @@
+import Appbar from "../components/Appbar"
+
+
+const Mainpage = () => {
+  return (
+    <div className="bg-gray-200">
+        <Appbar/>
+    </div>
+  )
+}
+
+export default Mainpage
